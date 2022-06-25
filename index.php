24a2100f30457f56134754326213c4345dbd939d
mohammedterfa <mohammedterfa@gmail.com>
2022-06-25 17:02:03 +0200

    PHP whitespace insensitivity

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
  echo "Defult syntax";
+
+
+ $amount = 200;
+
+ echo("<br>The amount is :  $amount <br>");
+
+ function student_info($student_name , $class, $roll_no){
+    echo "the name of student : $student_name <br>";
+    echo "His Class is : $class and Roll no: $roll_no <br>";
+
+ }
+
+ student_info("Mohammed Terfa" , "V", 12);
+
 ?>
