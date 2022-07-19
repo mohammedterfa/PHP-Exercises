96a095672df3f19ef67022f420eb95536b74c8b2
mohammedterfa <mohammedterfa@gmail.com>
2022-07-19 14:17:57 +0200

    Single line and Multiple lines Comments

--- index.php
+++ index.php
@@ -1,31 +1,8 @@
 <?php
- echo "Defult syntax";
+ echo "How to make single line comment";
 
+ #this is make single line comment#
+ //this is another way to make single line comment
 
- $amount = 200;
-
- echo("<br>The amount is :  $amount <br>");
-
- function student_info($student_name , $class, $roll_no){
-    echo "the name of student : $student_name <br>";
-    echo "His Class is : $class and Roll no: $roll_no <br>";
-
- }
-
- student_info("Mohammed Terfa" , "V", 12);
-
-// Advance whitespace insensitivity
- function student_infoo(
-    $StudentName,
-    $classNum,
-    $Roll
- ){
-    echo "the name of student : $StudentName <br>";
-    echo "His Class is : $classNum and Roll no: $Roll <br>";
- }
-
- student_infoo(
-    "Terfa Mohamed", "R" ,14
- );
-
+ 
 ?>
