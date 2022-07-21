24e340e40b3c8059bfa13a6d79b342b598a22ffb
mohammedterfa <mohammedterfa@gmail.com>
2022-07-21 22:20:29 +0200

    Add some value into the title section within an HTML document

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
-echo "Tomorrow I \'ll learn PHP global variables."."\n"; 
-echo "This is a bad command : del c:\\*.*"."\n"; 
+$var = 'PHP Tutorial';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3><?php echo $var; ?></h3>
+</body>
+</html>
