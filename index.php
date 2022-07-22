03c5a2369c4078f8554424515349c11c341a09e4
mohammedterfa <mohammedterfa@gmail.com>
2022-07-22 21:37:02 +0200

    Create a HTML form and accept the user name and display the name

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
-<?php
-$var = 'PHP Tutorial';
-?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h3><?php echo $var; ?></h3>
+   <title></title>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+   </head>
+   <body>
+   <form method='POST'>
+   <h2>Please input your name:</h2>
+ <input type="text" name="name">
+ <input type="submit" value="Submit Name">
+ </form>
+<?php
+//Retrieve name from query string and store to a local variable
+$name = $_POST['name'];
+echo "<h3> Hello $name </h3>";
+?>
 </body>
 </html>
