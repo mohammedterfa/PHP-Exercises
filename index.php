ef62710dfca5e73dc83db4f4dd8c7a0e96c481ed
mohammedterfa <mohammedterfa@gmail.com>
2022-07-25 13:18:08 +0200

    Return some components of an url

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-echo "Your User Agent is :" . $_SERVER ['HTTP_USER_AGENT'];
+$url = 'https://www.w3resource.com/php-exercises/php-basic-exercises.php';
+$url=parse_url($url);
+echo 'Scheme : '.$url['scheme']."\n";
+echo 'Host : '.$url['host']."\n";
+echo 'Path : '.$url['path']."\n";
 ?>
